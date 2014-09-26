3c8eca76d31fe9bd506fd925c23561827e8c30dd
anishikawa <anishikawa@01b10b89-4895-334c-a4a2-fda98f01737a>
2014-09-26 13:30:34 +0000

    Importación inicial del proyecto, no funcional.

---
+++ PID-CELTIC/infraestructura/opennebula/hve/trunk/src/main/java/es/indra/innovationlabs/hadoop/net/Cluster.java
@@ -0,0 +1,13 @@
+package es.indra.innovationlabs.hadoop.net;
+
+import java.util.List;
+
+class Cluster {
+
+    private List<Host> hosts ;
+    
+    public Cluster() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ PID-CELTIC/infraestructura/opennebula/hve/trunk/src/main/java/es/indra/innovationlabs/hadoop/net/Datacenter.java
@@ -0,0 +1,13 @@
+package es.indra.innovationlabs.hadoop.net;
+
+import java.util.List;
+
+class Datacenter {
+
+    private List<Cluster> clusters ;
+    
+    public Datacenter() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
